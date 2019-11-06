557c1fd4ba4829cf5d674d307e0d3a7e1786047d
ReadySetTest <adamp@retailsuccess.com>
2019-11-06 17:22:37 -0600

    Added initial project and ChracterModel

---
+++ Path.Character/Path.Character/Models/CharacterModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Path.Character.Models
+{
+    public class CharacterModel
+    {
+        public int Id { get; set; }
+        public int Age { get; set; }
+        public int Level { get; set; }
+        public int AbilitiesId { get; set; }
+        public int RaceId { get; set; }
+        public int TraitsId { get; set; }
+        public int AlignmentId { get; set; }
+        public int AccountId { get; set; }
+
+        public string CharacterName { get; set; }
+    }
+}
